766142dfbd758e34cbe310731ca7372345a5a426
Andrew <Asaganda@gmail.com>
2022-12-23 11:41:15 -0500

    handle create function and add restaurant form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 import './App.css';
+import Add from './components/Add';
 
 const App = () => {
+  // States
+  const [restaurants, setRestaurants] = useState([])
+
+  const handleCreate = (newRest) => {
+    axios.post("https://restaurant-api.herokuapp.com/api/restaurants", newRest)
+    .then((res) => {
+      console.log(res.data)
+      setRestaurants([...restaurants, res.data])
+    })
+  }
+
   return (
-    <h1>Restaurant App</h1>
+    <>
+      <h1>Restaurant App</h1>
+      <button>Add Restaurant</button>
+      <Add handleCreate={handleCreate} />
+    </>
   );
 }
 
