9ab8f8f0bce3d9948d2650d515683731a3ecd662
Andrew <Asaganda@gmail.com>
2024-01-31 22:24:01 -0500

    refactored styling, removed unused hook

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,7 +1,4 @@
-import React, { useState } from 'react'
-
 const Nav = (props) => {
-
 
     const showPage = (num) => {
     props.setPage(num)
@@ -9,23 +6,23 @@ const Nav = (props) => {
 
     return (
     <>
-<nav className="navbar bg-body-tertiary">
-    <div className="container-fluid">
-        <div>
-            <img className="mb-4 nav-logo" src="https://cdn-icons-png.flaticon.com/512/52/52172.png" alt="" onClick={() => showPage(0)}></img>
-            <a className="navbar-brand" href="#" onClick={() => showPage(0)}>Restaurant Reviewer</a>
-        </div>
-        {/* <div className="dropdown">
-            <button className="btn menu-button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Menu
-            </button>
-            <ul className="dropdown-menu">
-                <li><a className='dropdown-item' onClick={() => showPage(0)}>Home</a></li>
-                <li><a className='dropdown-item' onClick={() => showPage(1)}>Add </a></li>
-            </ul>
-        </div> */}
-    </div>
-</nav>
+        <nav className="navbar bg-body-tertiary">
+            <div className="container-fluid">
+                <div>
+                    <img className="mb-4 nav-logo" src="https://cdn-icons-png.flaticon.com/512/52/52172.png" alt="" onClick={() => showPage(0)}></img>
+                    <li className="navbar-brand d-inline" onClick={() => showPage(0)}>Restaurant Reviewer</li>
+                </div>
+                <div className="dropdown">
+                    <button className="btn menu-button dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Menu
+                    </button>
+                    <ul className="dropdown-menu">
+                        <li><button className='dropdown-item' onClick={() => showPage(0)}>Home</button></li>
+                        <li><button className='dropdown-item' onClick={() => showPage(1)}>Add </button></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </>
     )
 }
